81002d6ed1a124f7ba46038b1812787607a7fc93
Nezar Janahi <nezar.janahi@skiplino.com>
2023-11-28 16:20:14 +0300

    Update app.js

--- app.js
+++ app.js
@@ -122,7 +122,7 @@ function handleOrientation(event) {
     document.querySelector('.gamma').innerHTML = gamma 
 
     // Rotate Ticket
-    ticket.style.transform = `perspective(600px) rotateX(${0}deg) rotateY(${gamma}deg)`;
+    ticket.style.transform = `perspective(600px) rotateX(${gamma}deg) rotateY(${0}deg)`;
 
 }
  
