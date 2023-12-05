3b6137c5f985923c15f63b60e24f5c29b2415f46
Nezar Janahi <nezar.janahi@skiplino.com>
2023-12-05 17:46:00 +0300

    Update app.js

--- app.js
+++ app.js
@@ -165,11 +165,22 @@ function handleOrientation(event) {
     
     if(enable3d.checked) {
 
+        document.querySelector('.dev-ticket').style = `
+        -webkit-transform: perspective(600px) rotateX(${beta / 8}deg) rotateY(${gamma / 3}deg);
+        transform: perspective(600px) rotateX(${beta / 8}deg) rotateY(${gamma / 3}deg);
+        `
+
         ticket.style = `
         -webkit-transform: perspective(600px) rotateX(${beta / 8}deg) rotateY(${gamma / 3}deg);
                 transform: perspective(600px) rotateX(${beta / 8}deg) rotateY(${gamma / 3}deg);
         `;
     } else {
+
+        document.querySelector('.dev-ticket').style = `
+        -webkit-transform: perspective(600px) rotateX(0deg) rotateY(0deg);
+                transform: perspective(600px) rotateX(0deg) rotateY(0deg);
+        `
+
 
         ticket.style = `
         -webkit-transform: perspective(600px) rotateX(0deg) rotateY(0deg);
